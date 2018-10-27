0bdd61e09053a418c4ee36bca326d7e638cc9e9e
Yaroslav Belgorosky <yshchukin@yandex.ru>
2018-10-28 02:01:49 +0300

    3.7 solutions

---
+++ 03/string_5.cpp
@@ -0,0 +1,25 @@
+#include <algorithm> // std::swap
+#include <cstddef>   // size_t
+#include <cstring>   // strlen, strcpy
+
+struct String {
+	String(const char *str = "");
+	String(size_t n, char c);
+	String(const String &other);
+	~String();
+
+	/* Реализуйте оператор присваивания */
+	String &operator=(const String &other)
+	{
+		if(this != &other) {
+			this->~String();
+			new (this) String(other.str);
+		}
+		return *this;
+	}
+
+	void append(const String &other);
+
+	size_t size;
+	char *str;
+};
