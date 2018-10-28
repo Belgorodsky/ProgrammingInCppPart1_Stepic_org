19e51b6b82fbf5d0785fe880d8afee7838f67404
Yaroslav Belgorosky <yshchukin@yandex.ru>
2018-10-28 15:43:44 +0300

    4.1 solutions

---
+++ 04/character.hpp
@@ -0,0 +1,28 @@
+#pragma once
+#include <string> // std::string
+
+struct Character
+{
+    std::string const & name() const;
+    unsigned health() const;
+};
+
+struct LongRange : Character
+{
+    unsigned range() const;
+};
+
+struct SwordsMan : Character
+{
+    unsigned strength() const;
+};
+
+struct Wizard : LongRange
+{
+    unsigned mana() const;
+};
+
+struct Archer : LongRange
+{
+    unsigned accuracy() const;
+};
