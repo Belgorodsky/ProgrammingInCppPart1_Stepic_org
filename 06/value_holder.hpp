457d3f62c5ea35db146270d20e07a47788529a9d
Yaroslav Belgorosky <yshchukin@yandex.ru>
2018-10-31 14:37:30 +0300

    6.1: ValueHolder solution

---
+++ 06/value_holder.hpp
@@ -0,0 +1,32 @@
+#pragma once
+struct ICloneable
+{
+	virtual ICloneable* clone() const = 0;
+	virtual ~ICloneable() { }
+};
+
+// Шаблон ValueHolder с типовым параметром T,
+// должен содержать одно открытое поле data_
+// типа T.
+//
+// В шаблоне ValueHolder должен быть определен
+// конструктор от одного параметра типа T,
+// который инициализирует поле data_.
+//
+// Шаблон ValueHolder должен реализовывать
+// интерфейс ICloneable, и возвращать указатель
+// на копию объекта, созданную в куче, из метода
+// clone.
+
+template <typename T>
+struct ValueHolder : ICloneable
+{
+	ValueHolder(const T&val) : data_(val) {}
+
+	ValueHolder* clone() const
+	{
+		return new ValueHolder(data_);
+	}
+
+	T data_;
+};
